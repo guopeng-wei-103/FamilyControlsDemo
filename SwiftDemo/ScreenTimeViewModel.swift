2de1c2a19257fec5661f3cf0952af71cf4ef56eb
guopeng <guopeng.wei@foxmail.com>
2025-02-27 22:26:22 +0800

    a

---
+++ SwiftDemo/ScreenTimeViewModel.swift
@@ -0,0 +1,91 @@
+import Foundation
+import FamilyControls
+import DeviceActivity
+import ManagedSettings
+
+class ScreenTimeViewModel: ObservableObject {
+    @Published var activitySelection = FamilyActivitySelection()
+
+    private let encoder = PropertyListEncoder()
+    private let decoder = PropertyListDecoder()
+    private let userDefaultsKey = "ScreenTimeSelection"
+    
+    init() {
+        // Load saved selection when the view model is initialized
+        if let savedSelection = savedSelection() {
+            activitySelection = savedSelection
+        }
+    }
+    
+    func selectApps() {
+          let picker = FamilyActivityPicker(selection: activitySelection)
+          if let scene = UIApplication.shared.connectedScenes.first,
+             let windowScene = scene as? UIWindowScene {
+              windowScene.windows.first?.rootViewController?.present(picker, animated: true, completion: nil)
+          }
+      }
+
+    // saves all the selections passed into activitySelection
+    func saveSelection() {
+        let defaults = UserDefaults.standard
+        if let encodedSelection = try? encoder.encode(activitySelection) {
+            defaults.set(encodedSelection, forKey: userDefaultsKey)
+        }
+    }
+
+    // returns the last selected activities from user Defaults
+    func savedSelection() -> FamilyActivitySelection? {
+        let defaults = UserDefaults.standard
+        guard let data = defaults.data(forKey: userDefaultsKey) else { return nil }
+
+        if let decodedSelection = try? decoder.decode(FamilyActivitySelection.self, from: data) {
+            return decodedSelection
+        } else {
+            return nil
+        }
+    }
+    
+    func endMonitoring(_ id: String) { // stops monitoring screentime for specific pod
+        let center = DeviceActivityCenter()
+        center.stopMonitoring([DeviceActivityName(id)])
+    }
+    
+    func beginMonitoring(_ id: String) {
+        let schedule = DeviceActivitySchedule(
+            intervalStart: DateComponents(hour: 0, minute: 0, second: 0),
+            intervalEnd: DateComponents(hour: 23, minute: 59, second: 59),
+            repeats: true
+        )
+        
+        guard let selection: FamilyActivitySelection = self.savedSelection() else {
+            print("beginMonitoring(): saved selection not found")
+            return
+        }
+        
+        let event = DeviceActivityEvent(
+            applications: selection.applicationTokens,
+            categories: selection.categoryTokens,
+            webDomains: selection.webDomainTokens,
+            threshold: DateComponents(hour: 10)
+        )
+        
+        let center = DeviceActivityCenter()
+        
+        // monitors screentime for each pod
+        let activity = DeviceActivityName(id)
+        let eventName = DeviceActivityEvent.Name("ScreenTimeMonitoring")
+        
+        do {
+            try center.startMonitoring(
+                activity,
+                during: schedule,
+                events: [
+                    eventName: event
+                ]
+            )
+        } catch {
+            print("beginMonitoring(): Error starting ScreenTimeMonitoring")
+        }
+        
+    }
+}
